c811c80cf53df40416671b2dd1dba6e181ac8738
romptroll <romptroll@hotmail.com>
2020-11-22 11:21:40 +0100

    renamed to engine_core

--- src/lib.rs
+++ src/lib.rs
@@ -21,55 +21,9 @@
  *   SOFTWARE.
  */
 
-#[macro_export]
-macro_rules! gl_call  {
-    ($x:expr) => {
-        crate::renderer::gl_clear_error();
-        $x;
-        if crate::renderer::gl_log_call(file!(), line!()) == false {
-            
-        }
-    };
-}
+#[macro_use]
+pub mod log;
 
-#[macro_export]
-macro_rules! info_log {
-    () => (crate::info(&format!("[File] {} [Line] {}", file!(), line!())));
-    ($($arg:tt)*) => ({
-        crate::info(&format!("[File] {} [Line] {} [Message] {}", file!(), line!(), format!($($arg)*)));
-    })
-}
-
-#[macro_export]
-macro_rules! warn_log {
-    () => (crate::warn(&format!("[File] {} [Line] {}", file!(), line!())));
-    ($($arg:tt)*) => ({
-        crate::warn(&format!("[File] {} [Line] {} [Message] {}", file!(), line!(), format!($($arg)*)));
-    })
-}
-
-#[macro_export]
-macro_rules! error_log {
-    () => (crate::error(&format!("[File] {} [Line] {}", file!(), line!())));
-    ($($arg:tt)*) => ({
-        crate::error(&format!("[File] {} [Line] {} [Message] {}", file!(), line!(), format!($($arg)*)));
-    })
-}
-
-#[macro_export]
-macro_rules! fatal_log {
-    () => (crate::fatal(&format!("[File] {} [Line] {}", file!(), line!())));
-    ($($arg:tt)*) => ({
-        crate::fatal(&format!("[File] {} [Line] {} [Message] {}", file!(), line!(), format!($($arg)*)));
-    })
-}
-
-use colored::*;
-
-pub fn info(message: &str)      { println!("{}", format!("[INFO] {}", message).green().bold())  }
-pub fn warn(message: &str)      { println!("{}", format!("[WARN] {}", message).yellow().bold()) }
-pub fn error(message: &str)     { println!("{}", format!("[ERROR] {}", message).red().bold())   }
-pub fn fatal(message: &str)     { println!("{}", format!("[FATAL] {}", message).red().bold())   }
 
 #[cfg(test)]
 mod tests {
